0dbd6d314687285839a5c59d43c8c2db9b217fd5
Henry-Freck <henry.freck@gmail.com>
2021-09-10 20:21:47 -0400

    added late schedule function

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from classes import course
 from non_member_methods import generate_early_schedule
+from non_member_methods import generate_late_schedule
 def main():
     possible_courses = []
     course_code = "not empty"
+
+    num_courses = int(input("How many courses would you like in the final schedule?: "))
 
     while course_code != "":
         course_code = input("Enter course code, or enter to finish: ")
@@ -10,12 +13,15 @@ def main():
         if course_code == "":
             break
 
+        start_time = int(input("Enter this sections start time: "))
         end_time = int(input("Enter this section's end time: "))
-        new_course = course(course_code, end_time = end_time)
+        new_course = course(course_code, end_time = end_time, start_time = start_time)
         possible_courses.append(new_course)
 
-    completed_schedule = generate_early_schedule(possible_courses, 1) #TODO dont hardcode this
+    completed_schedule = generate_late_schedule(possible_courses, num_courses)
+
     for course_instance in completed_schedule:
         print(course_instance)
+
 if __name__ == "__main__":
     main()
--- non_member_methods.py
+++ non_member_methods.py
@@ -4,3 +4,8 @@ def generate_early_schedule(possible_courses, num_courses):
     possible_courses.sort(key = lambda course: course.end_time)
     chosen_courses = possible_courses[0:num_courses]
     return chosen_courses
+
+def generate_late_schedule(possible_courses, num_courses):
+    possible_courses.sort(key = lambda course: course.start_time)
+    chosen_courses = possible_courses[len(possible_courses)-num_courses:]
+    return chosen_courses
